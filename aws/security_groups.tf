6f1a7aacf3f4efa3668c1af5c43065111a420b89
Rinat Mustafaev <rinat.mustafaev@konux.de>
2024-05-06 19:17:17 +0200

    Initialisation via aws and terraform

---
+++ aws/security_groups.tf
@@ -0,0 +1,52 @@
+locals {
+  security_groups = {
+    allow_tls = {
+      description = "Allow TLS inbound traffic"
+      ingress = {
+        all_tls = {
+          from_port   = 443
+          to_port     = 443
+          ip_protocol = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+        }
+      }
+    }
+    allow_tls_bw_instances = {
+      description = "Allow TLS inbound traffic bw instances"
+      ingress = {
+        self_tls = {
+          from_port   = 443
+          to_port     = 443
+          ip_protocol = "tcp"
+          self        = true
+        }
+      }
+    }
+  }
+}
+
+resource "aws_security_group" "this" {
+  for_each    = local.security_groups
+  name        = each.key
+  description = each.value.description
+  vpc_id      = module.vpc.vpc_id
+  dynamic "ingress" {
+    for_each = each.value.ingress
+    content {
+      from_port   = ingress.value.from_port
+      to_port     = ingress.value.to_port
+      protocol    = ingress.value.ip_protocol
+      self        = try(ingress.value.self, false)
+      cidr_blocks = try(ingress.value.cidr_blocks, [])
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [module.vpc.vpc_cidr_block]
+  }
+  tags = {
+    Name = each.key
+  }
+}
